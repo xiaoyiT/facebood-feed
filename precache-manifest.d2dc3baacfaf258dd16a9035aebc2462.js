f2bbc23225f56de03920bd1279e17976984407c9
Xiaoyi Tang <xt59@scarletmail.rutgers.edu>
2019-04-30 00:56:01 -0700

    Updates

---
+++ precache-manifest.d2dc3baacfaf258dd16a9035aebc2462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd1e4edb534228e627cf580b9ea7387a",
+    "url": "/facebook-feed/index.html"
+  },
+  {
+    "revision": "de5498bb85952a8d71c5",
+    "url": "/facebook-feed/static/css/main.e1dd7326.chunk.css"
+  },
+  {
+    "revision": "0440f5c61b89d51833e3",
+    "url": "/facebook-feed/static/js/2.97294558.chunk.js"
+  },
+  {
+    "revision": "de5498bb85952a8d71c5",
+    "url": "/facebook-feed/static/js/main.bddcfc9f.chunk.js"
+  },
+  {
+    "revision": "8c3916177e3f1421b871",
+    "url": "/facebook-feed/static/js/runtime~main.941e829f.js"
+  }
+]);
